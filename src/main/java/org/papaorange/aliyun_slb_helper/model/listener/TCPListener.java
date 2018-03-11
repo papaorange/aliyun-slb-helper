ebb6f45efa1f605d19cda2b728f3931619e27718
papaorange <13995656568@163.com>
2018-03-12 01:53:59 +0800

    format

--- src/main/java/org/papaorange/aliyun_slb_helper/model/listener/TCPListener.java
+++ src/main/java/org/papaorange/aliyun_slb_helper/model/listener/TCPListener.java
@@ -2,24 +2,23 @@ package org.papaorange.aliyun_slb_helper.model.listener;
 
 public class TCPListener extends Listener {
 
-  private Integer persistenceTimeout;
-  private Integer establishedTimeout;
+	private Integer	persistenceTimeout;
+	private Integer	establishedTimeout;
 
-  public Integer getPersistenceTimeout() {
-    return persistenceTimeout;
-  }
+	public Integer getPersistenceTimeout() {
+		return persistenceTimeout;
+	}
 
-  public void setPersistenceTimeout(Integer persistenceTimeout) {
-    this.persistenceTimeout = persistenceTimeout;
-  }
+	public void setPersistenceTimeout(Integer persistenceTimeout) {
+		this.persistenceTimeout = persistenceTimeout;
+	}
 
-  public Integer getEstablishedTimeout() {
-    return establishedTimeout;
-  }
+	public Integer getEstablishedTimeout() {
+		return establishedTimeout;
+	}
 
-  public void setEstablishedTimeout(Integer establishedTimeout) {
-    this.establishedTimeout = establishedTimeout;
-  }
-
+	public void setEstablishedTimeout(Integer establishedTimeout) {
+		this.establishedTimeout = establishedTimeout;
+	}
 
 }
--- src/test/java/org/papaorange/aliyun_slb_helper/AppTest.java
+++ src/test/java/org/papaorange/aliyun_slb_helper/AppTest.java
@@ -16,7 +16,7 @@ public class AppTest extends TestCase {
 	 */
 	public AppTest(String testName) {
 		super(testName);
-	    
+
 	}
 
 	/**
